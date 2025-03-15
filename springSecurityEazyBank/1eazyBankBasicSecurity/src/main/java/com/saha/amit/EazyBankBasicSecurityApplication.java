198c8652350ca04079528d8008079f7576cf681f
justamitsaha <justamitsaha@gmail.com>
2025-03-15 14:46:02 +0530

    Doc updated

--- springSecurityEazyBank/1eazyBankBasicSecurity/src/main/java/com/saha/amit/EazyBankBasicSecurityApplication.java
+++ springSecurityEazyBank/1eazyBankBasicSecurity/src/main/java/com/saha/amit/EazyBankBasicSecurityApplication.java
@@ -1,13 +1,17 @@
 package com.saha.amit;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EazyBankBasicSecurityApplication {
 
+    public static final Log log = LogFactory.getLog(EazyBankBasicSecurityApplication.class);
+
     public static void main(String[] args) {
-        System.out.println("http://localhost:8080/public/home");
+        log.info("http://localhost:8080/public/home");
         SpringApplication.run(EazyBankBasicSecurityApplication.class, args);
     }
 
